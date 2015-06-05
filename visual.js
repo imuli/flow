e78c095dbca767a830903ae70a25c568befdc13c
Imuli <i@imu.li>
2015-06-05 02:09:47 -0400

    prettier and automatic.

--- visual.js
+++ visual.js
@@ -1,128 +1,61 @@
 var flow = (function(){
-	/* to calculate cells spans properly */
-	function gcf(a, b){
-		if(b == 0) return a;
-		a = a % b;
-		return gcf(b, a);
-	}
-	function lcm(a, b){
-		return a*b/gcf(a,b);
-	}
-
 	/* convenience */
 	function elementSVG(tag){
 		return document.createElementNS("http://www.w3.org/2000/svg", tag);
 	}
-	function elementHTML(tag){
-		return document.createElementNS("http://www.w3.org/1999/xhtml", tag);
-	}
-
-	/* user interaction */
-	function move(e){
-		var node = this.parentNode;
-		var xoffset = e.clientX - node.x.baseVal.value;
-		var yoffset = e.clientY - node.y.baseVal.value;
-		var movement = function(e){
-			node.x.baseVal.value = e.clientX - xoffset;
-			node.y.baseVal.value = e.clientY - yoffset;
-		}
-		window.addEventListener("mousemove", movement);
-		window.addEventListener("mouseup", function cleanup (e){
-			window.removeEventListener("mousemove", movement);
-			window.removeEventListener("mouseup", cleanup);
-		});
-		return false;
+	function box(id, dir){
+		var box = elementSVG("g");
+		box.id = id;
+		box.setAttribute("data-bind", "tight");
+		box.setAttribute("data-dir", dir);
+		return box;
 	}
 
 	/* make elements */
-	function cell(node, id, rows){
-		var td = elementHTML(node.type === undefined ? "th" : "td");
-		var text = document.createTextNode(node.name);
-		td.appendChild(text);
-		td.setAttribute("rowspan", rows);
-		td.setAttribute("id", id);
-		if(node.type !== undefined){
-			td.setAttribute("class", node.type);
+	function ioCell(io, id){
+		var g = elementSVG("g");
+		g.id = id;
+		if(io.type !== undefined){
+			g.classList.add(io.type);
 		}
-		if(node.description !== undefined){
-			td.setAttribute("title", node.description);
+		if(io.description !== undefined){
+			var title = elementSVG("title");
+			g.appendChild(title);
+			var titletext = document.createTextNode(io.description);
+			title.appendChild(titletext);
 		}
-		return td;
+		var rect = elementSVG("rect");
+		g.appendChild(rect);
+		var text = elementSVG("text");
+		g.appendChild(text);
+		var node = document.createTextNode(io.name);
+		text.appendChild(node);
+		return g;
 	}
-	function row(cols, id, n, total){
-		var row = elementHTML("tr");
-		for(i in cols){
-			var span = total/cols[i].length;
-			if(n % span == 0){
-				row.appendChild(cell(cols[i][n/span],
-							id + '.' + i + "." + n/span, span));
-			}
-		}
-		return row;
+	function ioBoxes(ios, id){
+		var me = box(id, "v");
+		ios.map(function(io, n){
+			me.appendChild(ioCell(io, id + "." + n));
+		});
+		return me;
 	}
-	function inner(func, id){
-			var table = elementHTML("table");
-			var rowCount = lcm(func.in.length, func.out.length);
-			var cols = {in: func.in, body: [func], out: func.out};
-			for(var i = 0; i < rowCount; i++){
-				table.appendChild(row(cols, id, i, rowCount));
-			}
-			table.setAttribute("id", id);
-			table.setAttribute("class", "fn");
-			table.addEventListener("mousedown", move);
-			return table;
+	function bodyBox(fn, id){
+		var me = ioCell(fn, id);
+		me.classList.add("body");
+		return me;
 	}
-
-	function sumMeasure(node, measure){
-		if(node.tagName == 'svg') return 0;
-		return node[measure] + sumMeasure(node.parentNode, measure);
-	}
-	function link(spec){
-		fr = document.getElementById(this + "." + spec.fr);
-		to = document.getElementById(this + "." + spec.to);
-		var link = elementSVG("path");
-		var frbase = fr.parentNode.parentNode.parentNode;
-		var tobase = to.parentNode.parentNode.parentNode;
-		var frx = frbase.offsetLeft + fr.offsetLeft + fr.offsetWidth;
-		var fry = frbase.offsetTop + fr.offsetTop + fr.offsetHeight/2;
-		var tox = tobase.offsetLeft + to.offsetLeft;
-		var toy = tobase.offsetTop + to.offsetTop + to.offsetHeight/2;
-		link.setAttribute("d", "M" + frx + " " + fry + "L" + tox + " " + toy);
-		return link;
+	function view(func, id){
+		var me = box(id, "h");
+		me.classList.add("fn");
+		me.appendChild(ioBoxes(func.in, id + '.in'));
+		me.appendChild(bodyBox(func, id + '.body'));
+		me.appendChild(ioBoxes(func.out, id + '.out'));
+		return me;
 	}
 
 	return {
-		View : function(func, id, par){
-			var g = elementSVG("foreignObject");
-			var table = inner(func, id);
-			g.appendChild(table);
-			par.appendChild(g);
-			g.setAttribute("width", table.offsetWidth);
-			g.setAttribute("height", table.offsetHeight);
-			return {
-				me: g,
-			}
-		},
-		BigView : function(func, fns, id, par){
-			var g = elementSVG("foreignObject");
-			var table = inner(func, id);
-			g.appendChild(table);
-			par.appendChild(g);
-			table.classList.add("bigfn");
-			g.setAttribute("width", table.offsetWidth);
-			g.setAttribute("height", table.offsetHeight);
-			var fns = func.fns.map(function(x, i){
-				var s = flow.View(fns[x.ref], id + '.' + i, par);
-				s.me.setAttribute("x", 80);
-				s.me.setAttribute("y", 80 + 80*i);
-				return s;
-			});
-			var map = func.map.map(link, id);
-			map.map(function(x){par.appendChild(x)});
-			return {
-				me: g,
-				fns: fns,
-			}
+		View : function(func, id){
+			return view(func, id);
 		},
 	}
 })();
@@ -174,7 +107,10 @@ var me;
 var svg;
 function draw(){
 	svg = document.getElementsByTagName("svg")[0];
-	example = flow.BigView(fns["5165da0e"], fns, "5165da0e", svg);
+	example = flow.View(fns["5165da0e"], "5165da0e");
+	svg.appendChild(example);
+	normalize(example);
+	example.setAttribute("transform", "translate(20, 20)");
 }
 
 function getMinBoxes(node){
